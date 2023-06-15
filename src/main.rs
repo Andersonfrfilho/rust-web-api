d660401ae9ef46f0696383c9294d7ffab7f75233
Anderson Fernandes <Anderson.filho@luizalabs.com>
2023-06-15 12:54:16 -0300

    feat: add logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod modules;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
 use modules::{health::controllers::users_scope_config, users::controllers::health_scope_config};
 
 #[get("/hello")]
@@ -22,9 +23,16 @@ fn config(cfg: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("##########9#");
-    HttpServer::new(|| App::new().configure(config))
-        .bind(("0.0.0.0", 3000))?
-        .run()
-        .await
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new(
+                "%P Authorization %{Authorization}i(0..2) %a %{User-Agent}i",
+            ))
+            .configure(config)
+    })
+    .bind(("0.0.0.0", 3000))?
+    .run()
+    .await
 }
