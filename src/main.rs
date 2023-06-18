c0779e0d0588746f652afc6c4f71064b61612d76
Anderson Fernandes <Anderson.filho@luizalabs.com>
2023-06-18 01:34:31 -0300

    feat: add log obfuscator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod modules;
-use actix_web::{
-    get, http::header::HeaderValue, middleware::Logger, web, App, HttpResponse, HttpServer,
-    Responder,
-};
+mod utils;
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_requestid::RequestIDMiddleware;
 use env_logger::Env;
 use modules::{health::controllers::users_scope_config, users::controllers::health_scope_config};
+use utils::obfuscator_part_of_value;
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
@@ -24,23 +24,20 @@ fn config(cfg: &mut web::ServiceConfig) {
         .service(hello_two);
 }
 
-fn parse_jwt_id(_req: Option<&HeaderValue>) -> String {
-    print!("#####-entrou no loger");
-    print!("{:?}", &_req);
-    "jwt_uid".to_owned()
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
+            .wrap(RequestIDMiddleware::default())
             .wrap(
-                Logger::new("%P Authorization %{Authorization}xi %a %{User-Agent}i")
-                    .exclude("/healthcheck")
-                    .custom_request_replace("Authorization", |req| {
-                        parse_jwt_id(req.headers().get("Authorization"))
-                    }),
+                Logger::new(
+                    "%Authorization: %{Authorization}xi request-id: %{request-id} %a %{User-Agent}i",
+                )
+                .exclude("/healthcheck")
+                .custom_request_replace("Authorization", |req| {
+                    obfuscator_part_of_value(req.headers().get("Authorization"))
+                }),
             )
             .configure(config)
     })
---
+++ src/utils.rs
@@ -0,0 +1,19 @@
+use actix_web::http::header::HeaderValue;
+
+pub fn obfuscator_part_of_value(header_value: Option<&HeaderValue>) -> String {
+    let value_string = match header_value {
+        None => panic!("value not defined"),
+        Some(header) => header.to_str().unwrap(),
+    };
+
+    if value_string.len() >= 9 {
+        let obfuscated_value = format!(
+            "{}***{}",
+            value_string[0..3].to_string(),
+            value_string[value_string.len() - 3..value_string.len()].to_string()
+        );
+        return obfuscated_value;
+    }
+
+    return "***".to_string();
+}
